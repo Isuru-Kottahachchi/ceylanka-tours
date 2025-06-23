9a1b24e84146df467141aba607b239e3bb7a1806
Isuru-Kottahachchi <isurukottasl@gmail.com>
2025-06-23 20:31:09 +0530

    added Measurement ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -101,7 +101,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX', {
+              gtag('config', 'G-9JYRWPZ8QK', {
                 page_title: document.title,
                 page_location: window.location.href,
               });
